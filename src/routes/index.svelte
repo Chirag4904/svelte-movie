e8c302572dca566b02d8803c7d5ab964a7c97aaa
chiggy69 <chiragaggarwal69@gmail.com>
2022-04-23 12:57:53 +0530

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/discover/movie?api_key=dbceed12e440d492620b9f8d466bdc87&language=en-US`
+		);
+		const data = await res.json();
+		console.log(res);
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<!-- //dbceed12e440d492620b9f8d466bdc87 -->
